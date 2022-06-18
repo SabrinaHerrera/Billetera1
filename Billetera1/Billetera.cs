1e71c44872541744ba8b97702e817b0c0a0b2578
Sabrina <sabrinaherrera199@gmail.com>
2022-06-18 19:53:41 -0300

    Agregar archivos de proyecto.

---
+++ Billetera1/Billetera.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Billetera1
+{
+    public class Billetera
+    {
+        public int BilleteDe10{ get; set; }
+        public int BilleteDe20 { get; set; }
+        public int BilleteDe50 { get; set; }
+        public int BilleteDe100 { get; set; }
+        public int BilleteDe200 { get; set; }
+        public int BilleteDe500 { get; set; }
+        public int BilleteDe1000 { get; set; }
+
+
+        public decimal Total()
+        {
+            decimal TotalBillete10 = BilleteDe10 * 10;
+            decimal TotalBillete20 = BilleteDe20 * 20;
+            decimal TotalBillete50 = BilleteDe50 * 50;
+            decimal TotalBillete100 = BilleteDe100 * 100;
+            decimal TotalBillete200 = BilleteDe200 * 200;
+            decimal TotalBillete500 = BilleteDe500 * 500;
+            decimal TotalBillete1000 = BilleteDe1000 * 1000;
+
+            decimal Total = TotalBillete10 + TotalBillete20 + TotalBillete50 + TotalBillete100 + TotalBillete200 + TotalBillete500 + TotalBillete1000;
+            return Total;
+        }
+
+        public  Billetera Combinar(Billetera miSegundaBilletera)
+        {
+            Billetera nuevaBilletera = new Billetera();
+
+            int SumaTotal10 = BilleteDe10 + miSegundaBilletera.BilleteDe10;
+            int SumaTotal20 = BilleteDe20 + miSegundaBilletera.BilleteDe20;
+            int SumaTotal50 = BilleteDe50 + miSegundaBilletera.BilleteDe50;
+            int SumaTotal100 = BilleteDe100 + miSegundaBilletera.BilleteDe100;
+            int SumaTotal200 = BilleteDe200 + miSegundaBilletera.BilleteDe200;
+            int SumaTotal500 = BilleteDe500 + miSegundaBilletera.BilleteDe500;
+            int SumaTotal1000 = BilleteDe1000 + miSegundaBilletera.BilleteDe1000;
+           
+            return nuevaBilletera;
+        }
+
+        
+    }
+}
---
+++ Billetera1/Program.cs
@@ -0,0 +1,29 @@
+﻿using Billetera1;
+
+Console.WriteLine("Estas son nuestras billeteras");
+
+//Primer Billetera
+
+Billetera b1 = new Billetera();
+b1.BilleteDe20 = 10;
+b1.BilleteDe1000 = 1;
+
+Console.WriteLine($"La billetera N° 1 tiene {b1.Total}");
+
+
+//Segunda Billetera
+
+Billetera b2 = new Billetera();
+b2.BilleteDe50 = 10;
+b2.BilleteDe500 = 2;
+b2.BilleteDe1000 = 3;
+
+//Terecer Billetera
+
+Billetera b3 = b1.Combinar(b1);
+
+
+
+
+
+
